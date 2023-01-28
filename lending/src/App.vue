b6f5adcb1c023142e72e97d0e4d56c8d475bac7e
Loran <danilmockhnatkin@gmail.com>
2023-01-28 20:32:54 +0400

    Данила Мохнаткин
    Небольшой апдейт

--- lending/src/App.vue
+++ lending/src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <Main />
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// import HelloWorld from './components/HelloWorld.vue'
+  import Main from './components/Main.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    Main
   }
 }
 </script>
@@ -21,6 +24,10 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
+}
+html, body{
+  padding: 0px;
+  margin: 0px;
 }
 </style>
---
+++ lending/src/components/Main.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="first_block">
+    <!-- <h3>First</h3> -->
+    <p class="textflow">JustSmile</p>
+  </div>
+  <div class="second_block">
+    <h3>Second</h3>
+  </div>
+</template>
+
+<script>
+export default{
+  name: 'main_page',
+  data(){
+    return{}
+  },
+  methods:{
+  }
+}
+</script>
+
+<style scoped>
+/* Первый блок информации */
+.first_block{
+  min-height: 100vh;
+  background: black;
+  /* border: solid black; */
+}
+
+.textflow{
+  font-size: 7em;
+  background: linear-gradient(90deg, #e933ff, #ffb400, #ff3333, #334dff, #e933ff);
+  background-size: 200% auto;
+  background-clip: text;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  animation: textflow 5s linear infinite;
+}
+@keyframes textflow {
+  from {background-position: 200% center}
+  to {background-position: 0% center}
+}
+
+.second_block{
+  min-height: 100vh;
+  background-color: blue;
+  /* border: solid black; */
+}
+
+.first_block, .second_block, h3, p{
+  margin: inherit;
+}
+</style>
