043525368870cba3548c71b0a06a64638fd12dc4
Loran <danilmockhnatkin@gmail.com>
2023-01-29 15:45:52 +0400

    Данила Мохнаткин
    Небольшое обновление

--- lending/src/components/Main.vue
+++ lending/src/components/Main.vue
@@ -2,8 +2,10 @@
   <div class="first_block">
     <!-- <h3>First</h3> -->
     <p class="textflow">JustSmile</p>
+    <p class="copyright" id="copyright">All rights reserved by L.R.N. in 2023 years ©</p>
+    <p class="navigation_first"> &#65088; </p>
   </div>
-  <div class="second_block">
+  <div class="second_block" id="second_block">
     <h3>Second</h3>
   </div>
 </template>
@@ -23,11 +25,17 @@ export default{
 /* Первый блок информации */
 .first_block{
   min-height: 100vh;
+  width: 100%;
+  user-select: none;
   background: black;
-  /* border: solid black; */
 }
 
 .textflow{
+  position: absolute;
+  width: inherit;
+  top: 50%;
+  transform: translateY(-50%);
+  margin: auto;
   font-size: 7em;
   background: linear-gradient(90deg, #e933ff, #ffb400, #ff3333, #334dff, #e933ff);
   background-size: 200% auto;
@@ -41,8 +49,30 @@ export default{
   to {background-position: 0% center}
 }
 
+.copyright{
+  color: #c0c0c0;
+  position: absolute;
+  width: inherit;
+  top: 99%;
+  transform: translateY(-99%);
+  margin: auto;
+}
+
+.navigation_first{
+  /* display: none; */
+  font-size: 5em;
+  height: 10%;
+  color: white;
+  position: absolute;
+  width: inherit;
+  top: 65%;
+  transform: translateY(-65%);
+  margin: auto;
+}
+
 .second_block{
   min-height: 100vh;
+  scroll-behavior: smooth;
   background-color: blue;
   /* border: solid black; */
 }
